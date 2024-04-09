57faa6d64546f3a6c8ce0c90b2271600a026c3ab
adamzales <127839305+adamzales@users.noreply.github.com>
2024-04-10 00:15:02 +0200

    Update config.lua

--- az_vehCrashSystem/config.lua
+++ az_vehCrashSystem/config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
 
-Config.Demage = 20 
+Config.Demage = 20 --If getting killed when hitting little things change this number
 
 Config.Speed = 50 -- Speed in MPH
 
-Config.Dispatch = "cd_dispatch"
+Config.Dispatch = "cd_dispatch" -- cd_dispatch
 
-Config.Jobs = {"police", "sheriff", "ambulance"}
+Config.Jobs = {"police", "sheriff", "ambulance"} --Jobs that gets the dispatch
